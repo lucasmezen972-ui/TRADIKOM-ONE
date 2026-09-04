e24356f951a46abaf8cd5f95a37362463f778c0f
TRADIKOM <TRADIKOM@MacBook-Pro-de-Lucas.local>
2026-09-04 14:37:17 -0400

    feat(conversation): add attachment access endpoint

--- src/modules/conversation-hub/index.ts
+++ src/modules/conversation-hub/index.ts
@@ -1,5 +1,6 @@
 export * from "@/modules/conversation-hub/errors";
 export * from "@/modules/conversation-hub/attachment-access";
+export * from "@/modules/conversation-hub/attachment-access-http";
 export * from "@/modules/conversation-hub/attachment-access-ticket";
 export * from "@/modules/conversation-hub/external-untrusted-data";
 export * from "@/modules/conversation-hub/repository";
